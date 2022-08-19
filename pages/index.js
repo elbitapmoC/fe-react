834424f35c3d5052a38af01ce71079051fefe574
Ahkal <bp@sojourn.llc>
2022-08-19 15:18:38 -0400

    Saves progress: - Adds button groups. - Renames a component - etc.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Footer from "../components/Footer";
-import MainArticle from "../components/MainArticle";
+import Champions from "../components/Champions";
 import Counter from "../components/Counter";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
@@ -18,11 +18,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Carousel showThumbs={false}>
-        <div className={styles.main}>
-          <MainArticle title="Random Champion Selection" />
+        <div className={`${styles.main}`}>
+          <Champions
+            title="Random Champion Selection"
+            description="Passing Props, useState &amp; Reactive Data"
+            filter={false}
+          />
         </div>
         <div className={styles.main}>
           <Counter title="Count Chocula" count={count} setCount={setCount} />
+        </div>
+        <div className="">
+          <Champions
+            title="Filtered List of Champion"
+            description="Passing Props, useState &amp; Reactive Data"
+            filter={true}
+          />
         </div>
       </Carousel>
       <Footer />
